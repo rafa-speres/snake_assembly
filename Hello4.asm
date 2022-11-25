3e89c21908d6abff784f6f84d9ecca7acad39ad7
Rafael Scalon Peres <65551999+rafa-speres@users.noreply.github.com>
2022-11-25 17:19:41 -0300

    Update Hello4.asm

--- Hello4.asm
+++ Hello4.asm
@@ -1,7 +1,28 @@
 ; Snake do Hexa
+
 ; Rafael Scalon Peres Conti - nUSP: 11871181
 ; Henrique Gualberto Marques - 
 ; João Otávio Cano - 
+
+; ------- TABELA DE CORES -------
+; adicione ao caracter para Selecionar a cor correspondente
+
+; 0 branco				0000 0000
+; 256 marrom			        0001 0000
+; 512 verde				0010 0000
+; 768 oliva				0011 0000
+; 1024 azul marinho		        0100 0000
+; 1280 roxo				0101 0000
+; 1536 teal				0110 0000
+; 1792 prata				0111 0000
+; 2048 cinza				1000 0000
+; 2304 vermelho				1001 0000
+; 2560 lima				1010 0000
+; 2816 amarelo				1011 0000
+; 3072 azul				1100 0000
+; 3328 rosa				1101 0000
+; 3584 aqua				1110 0000
+; 3840 branco	
 
 SnakePos:  	var #500
 SnakeSize:	var #1
